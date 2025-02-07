cc00da083ec612b8d087262abdd922462f1aeab5
Abdelilah <abdouajjouqa@gmail.com>
2025-02-07 17:29:17 +0100

    complet URL method

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -13,11 +13,13 @@ class Core {
         if(isset($_GET['url'])){
             $url = rtrim($_GET['url'], '/');
             $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
         }
     }
 
     public function __construct()
     {
-        $this->getUrl();
+        print_r($this->getUrl());
     }
 }
